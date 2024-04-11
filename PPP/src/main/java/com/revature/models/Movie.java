b158e484f334f703ff8c4d78c8b20eb709ae01e2
dmrichey <dmrichey@iu.edu>
2024-04-11 16:56:28 -0400

    Added Movie Class

---
+++ PPP/src/main/java/com/revature/models/Movie.java
@@ -0,0 +1,71 @@
+package com.revature.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+
+    // Data Fields
+
+    @Id
+    @Column (name = "userId")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+
+    private String genre;
+
+    private int releaseYear;
+
+
+    // Constructors
+
+    public Movie(String title, String genre, int releaseYear) {
+        this.title = title;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+    }
+
+    public Movie(int id, String title, String genre, int releaseYear) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+    }
+
+    // Getters and Setters
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+}
